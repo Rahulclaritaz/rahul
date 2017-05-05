f2d42dacf234d963a598670ca395482bbb5fe43c
Mathan <mathan.n@claritaz.com>
2017-05-05 14:54:20 +0530

    Welcome screen issue fixed

--- ixprez/ixprez/WelcomePageViewController.swift
+++ ixprez/ixprez/WelcomePageViewController.swift
@@ -1,178 +1,178 @@
-//
-//  WelcomePageViewController.swift
-//  ixprez
-//
-//  Created by Quad on 4/28/17.
-//  Copyright © 2017 Claritaz Techlabs. All rights reserved.
-//
+    //
+    //  WelcomePageViewController.swift
+    //  ixprez
+    //
+    //  Created by Quad on 4/28/17.
+    //  Copyright © 2017 Claritaz Techlabs. All rights reserved.
+    //
 
-import UIKit
+    import UIKit
 
-class WelcomePageViewController: UIPageViewController,UIPageViewControllerDataSource,UIPageViewControllerDelegate {
+    class WelcomePageViewController: UIPageViewController,UIPageViewControllerDataSource,UIPageViewControllerDelegate {
 
-    lazy var VCArray : [UIViewController]
-   = {
-        return [self.VCInstance(name: "FirstVC"),
-                self.VCInstance(name: "SecondVC"),
-                self.VCInstance(name: "ThirdVC")]
+        lazy var VCArray : [UIViewController]
+       = {
+            return [self.VCInstance(name: "FirstVC"),
+                    self.VCInstance(name: "SecondVC"),
+                    self.VCInstance(name: "ThirdVC"),
+                  ]
         
-    }()
-    
-    private func VCInstance(name : String) -> UIViewController
-    {
-        
-        return  UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: name)
-        
-        
-    }
-    
+        }()
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        self.delegate = self
-        self.dataSource = self
+        var index : Int!
+        var currentview  : Int! = 0
+        var perviouIndex : Int!
+        var nextIndex : Int!
         
-        if let firstVC = VCArray.first
+        var timer = Timer()
+        
+
+        private func VCInstance(name : String) -> UIViewController
         {
-            
-            setViewControllers([firstVC], direction: .forward, animated: true, completion: nil)
+            return  UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: name)
+
         }
         
-        
-        
-        // Do any additional setup after loading the view.
-    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-     func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController?
-    {
-        guard let viewControllerIndex = VCArray.index(of: viewController) else
-        {
-        return nil
-        }
-        
-        
-        let perviouIndex = viewControllerIndex - 1
-        
-        
-        guard perviouIndex >= 0 else
-        {
-            return VCArray.last
-        }
-        
-        
-        guard VCArray.count > perviouIndex else
-        {
-            return nil
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            self.delegate = self
+            self.dataSource = self
             
-        }
-        
-        
-        print(viewControllerIndex)
-        
-        return VCArray[perviouIndex]
-        
-    }
-    
-    
-     func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController?
-    {
-       
-        guard let viewControllerIndex = VCArray.index(of: viewController) else
-        {
-            return nil
-            
-        }
-        let nextIndex = viewControllerIndex + 1
-        
-        
-        guard nextIndex < VCArray.count else
-        {
-            /*let otpvc = self.storyboard?.instantiateViewController(withIdentifier: "OTPVerificationViewController") as! OTPVerificationViewController
-            
-            
-            self.present(otpvc, animated: true, completion: nil)
-             
-               return otpvc
-             */
-            
-            
-            return VCArray.first
-            
-          
-            
-        }
-        
-        guard VCArray.count > nextIndex else
-        {
-        
-            return nil
-            
-        }
-        
-        print(viewControllerIndex)
-        
-        return VCArray[nextIndex]
- 
-        
-    }
-
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-
-        
-        
-        for view in self.view.subviews
-        {
-            
-            if view is UIScrollView
+            if let firstVC = VCArray.first
             {
-                view.frame = UIScreen.main.bounds
+                setViewControllers([firstVC], direction: .forward, animated: true, completion:nil)
+           }
+        }
+        
+        override func didReceiveMemoryWarning() {
+            super.didReceiveMemoryWarning()
+            // Dispose of any resources that can be recreated.
+        }
+        
+         func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController?
+        {
+            guard let viewControllerIndex = VCArray.index(of: viewController) else
+            {
+            return nil
             }
-            else if view is UIPageControl
-            {
-                
-                view.backgroundColor = UIColor.clear
-                
-                
+            perviouIndex = viewControllerIndex - 1
 
-                
+            guard perviouIndex >= 0 else
+            {
+                return VCArray.last
+            }
+            
+            
+            guard VCArray.count > perviouIndex else
+            {
+                return nil
                 
             }
+            
+            
+            print(viewControllerIndex)
+            
+            return VCArray[perviouIndex]
+            
         }
         
-    }
-    
-    
-    
-      func presentationCount(for pageViewController: UIPageViewController) -> Int
-      {
+        
+         func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController?
+        {
+     
+     
+            guard let viewControllerIndex = VCArray.index(of: viewController) else
+            {
+                return nil
+     
+            }
+            nextIndex = viewControllerIndex + 1
+            
+            
+            currentview = nextIndex
+            print("now on tutorial slide",currentview)
+            
+            if currentview == 3
+            {
+                    let  nextViewController = self.storyboard?.instantiateViewController(withIdentifier: "RegistrationViewController") as! RegistrationViewController
+                    self.present(nextViewController, animated: false, completion: nil)
+            }
+            
+            let orderedViewControllersCount = VCArray.count
+            
+            
+           guard nextIndex < orderedViewControllersCount else
+            {
+                return VCArray.first
+            }
+            
+            guard orderedViewControllersCount != nextIndex else {
+                return nil
+            }
+            
+            guard orderedViewControllersCount > nextIndex else {
+                return nil
+            }
+            
+            return VCArray[nextIndex]
+            
+        }
       
-        return VCArray.count
         
-    }
-    
- 
-    func presentationIndex(for pageViewController: UIPageViewController) -> Int
-    {
-        guard let firstViewController = viewControllers?.first,
-            let firstViewControllerIndex = VCArray.index(of:firstViewController) else
-        {
-            return 0
+        override func viewDidLayoutSubviews() {
+            super.viewDidLayoutSubviews()
+
+            
+            
+            for view in self.view.subviews
+            {
+                
+                if view is UIScrollView
+                {
+                    view.frame = UIScreen.main.bounds
+                }
+                else if view is UIPageControl
+                {
+                    
+                    view.backgroundColor = UIColor.clear
+      
+                    
+                }
+            }
+            
         }
         
-        return firstViewControllerIndex
+        func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
+            
+         
+            
+        }
         
         
+        func presentationCount(for pageViewController: UIPageViewController) -> Int
+          {
+          
+            return VCArray.count
+            
+        }
         
-        
-    }
-    
-   
 
-}
+        
+        
+     
+        func presentationIndex(for pageViewController: UIPageViewController) -> Int
+        {
+            guard let firstViewController = viewControllers?.first,
+                let firstViewControllerIndex = VCArray.index(of:firstViewController) else
+            {
+                return 0
+            }
+            
+            return firstViewControllerIndex
+
+        }
+        
+       
+
+    }
