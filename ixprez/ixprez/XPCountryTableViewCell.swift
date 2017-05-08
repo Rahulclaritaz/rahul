369e00d55469f28cedb74f4a03f0e71153034e9f
Claritaz Techlabs <rahul@claritaz.com>
2017-05-08 20:29:57 +0530

    Worked on the autosuggested textfield registration

---
+++ ixprez/ixprez/XPCountryTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  XPCountryTableViewCell.swift
+//  ixprez
+//
+//  Created by Claritaz Techlabs on 08/05/17.
+//  Copyright © 2017 Claritaz Techlabs. All rights reserved.
+//
+
+import UIKit
+
+class XPCountryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
