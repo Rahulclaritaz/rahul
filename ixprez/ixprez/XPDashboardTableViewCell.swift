928aaca9150e37ef5417d2368b86d712fcfe5290
Claritaz Techlabs <rahul@claritaz.com>
2017-06-06 20:09:19 +0530

    Worked on the Trending Video

---
+++ ixprez/ixprez/XPDashboardTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  XPDashboardTableViewCell.swift
+//  ixprez
+//
+//  Created by Claritaz Techlabs on 06/06/17.
+//  Copyright © 2017 Claritaz Techlabs. All rights reserved.
+//
+
+import UIKit
+
+class XPDashboardTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
